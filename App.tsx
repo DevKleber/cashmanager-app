439cd016e291cf9bf6b3e23750db5e29cca39ece
thyerre <thyerre321@gmail.com>
2021-05-05 21:31:29 -0300

    criando tela de recuperar senha

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { ForgotPassword } from './src/components/pages/ForgotPassword';
 import { Login } from './src/components/pages/Login';
 import { NewAccount } from './src/components/pages/NewAccount';
 import { Splash } from './src/components/pages/Splash';
 
 const App = () => {
-  return <NewAccount />
+  return <ForgotPassword />
 };
 
 export default App;
---
+++ src/components/pages/ForgotPassword/style.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    display: flex;
+    flex: 1;
+    align-items: center;
+    padding-top: 14px;
+    background-color: #F5F6FC;
+`;
+
+export const Title = styled.Text`
+    margin-top: 30px;
+    font-size: 20px;
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 40px 40px;
+`;
+
+export const BtnEnviar = styled.TouchableOpacity`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    background-color: #00EB84;
+    color: #2A004F;
+    border-radius: 10px;
+    margin-top: 10px;
+    padding: 15px;
+    font-size: 15px;
+`;
+
+export const TextBtnEnviar = styled.Text`
+    font-size: 14px;
+    font-weight: bold;
+`;
+
+export const TextForgotPass = styled.Text``;
+
+
+export const ForgotPass = styled.TouchableOpacity`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    border-radius: 10px;
+    margin-top: 20px;
+`;
+export const TextBtnBackToLogin = styled.Text`
+    color: #00EB84;
+    margin-left: 7px;
+`;
+
+export const BtnNewAcount = styled.TouchableOpacity`
+    width: 100%;
+    height: 50px;
+    background-color: #2A004F;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+`;
