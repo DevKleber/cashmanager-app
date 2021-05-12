6dcaa3a2075683ed52174b718f5a559e5999c691
Kleber de Souza <kleber107@gmail.com>
2021-05-11 23:07:02 -0300

    Icones e estilos da tab

--- src/components/customTabBar/index.tsx
+++ src/components/customTabBar/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Icon} from 'react-native-elements/dist/icons/Icon';
+// import {Icon} from 'react-native-elements/dist/icons/Icon';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, TabItem, Name} from './styles';
+import {Container, TabItem, TabItemAdd, Name} from './styles';
 
 export function CustomTabBar({stack, navigation}: any) {
 	function handleGoTo(screenName: string) {
@@ -15,20 +16,32 @@ export function CustomTabBar({stack, navigation}: any) {
 				<Name>Início</Name>
 			</TabItem>
 			<TabItem onPress={() => handleGoTo('transacoes')}>
-				<Icon name="transacoes" size={33} color="#666666" />
+				<Icon name="receipt-long" size={33} color="#666666" />
 				<Name>Transações</Name>
 			</TabItem>
-			<TabItem onPress={() => handleGoTo('add')}>
-				<Icon name="add" size={33} color="#666666" />
-			</TabItem>
+			<TabItemAdd
+				style={style.boxShadow}
+				onPress={() => handleGoTo('add')}>
+				<Icon name="add" size={40} color="#fff" />
+			</TabItemAdd>
 			<TabItem onPress={() => handleGoTo('more')}>
-				<Icon name="more" size={33} color="#666666" />
+				<Icon name="outlined-flag" size={33} color="#666666" />
 				<Name>Planejamento</Name>
 			</TabItem>
 			<TabItem onPress={() => handleGoTo('more')}>
-				<Icon name="more" size={33} color="#666666" />
+				<Icon name="more-horiz" size={33} color="#666666" />
 				<Name>Mais</Name>
 			</TabItem>
 		</Container>
 	);
 }
+
+const style = {
+	boxShadow: {
+		shadowColor: '#000',
+		shadowOffset: {width: 0, height: 4},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 2,
+	},
+};
--- src/components/customTabBar/styles.ts
+++ src/components/customTabBar/styles.ts
@@ -2,14 +2,26 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
 	height: 68px;
-	background: #fff;
+	background: #f5f6fc;
 	flex-direction: row;
+	border-top-color: #dadada;
+	border-top-width: 1px;
 `;
 
 export const TabItem = styled.TouchableOpacity`
 	flex: 1;
 	justify-content: center;
 	align-items: center;
+`;
+export const TabItemAdd = styled.TouchableOpacity`
+	width: 65px;
+	height: 65px;
+	border-radius: 32.5px;
+	margin-top: -18px;
+
+	justify-content: center;
+	align-items: center;
+	background: #00eb84;
 `;
 export const Name = styled.Text`
 	font-size: 10px;
