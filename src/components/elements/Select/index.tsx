c40fa40089f6bb82b6a36d6814a44a95f5ed5c53
thyerre <thyerre321@gmail.com>
2021-05-19 23:11:42 -0300

    refactor in Select

--- src/components/elements/Select/index.tsx
+++ src/components/elements/Select/index.tsx
@@ -5,15 +5,15 @@ import {IconText} from '../Icon';
 
 import {Container, BoxIcon, TextPreFixer} from './style';
 
-function itemsTree(props: any) {
-	let element = [<Picker.Item label={props.placeholder} value='' color='#8b8b8b'/>];
+function itemsTree({placeholder, options, fields}: any) {
+	let element = [<Picker.Item label={placeholder} value='' color='#8b8b8b'/>];
 
-	props?.options?.map((item:any, index: number) => {
+	options?.map((item:any) => {
 
-		element = [...element, <Picker.Item key={index} label={item[props.fields.label]} value={item[props.fields.value]} color='#8b8b8b'/>];
+		element = [...element, <Picker.Item key={item.id} label={item[fields.label]} value={item[fields.value]} color='#8b8b8b'/>];
 
-		item?.children?.map((child:any, i: number) => {
-			element = [...element, <Picker.Item key={(i + index) * 100} label={child[props.fields.label]} value={child[props.fields.value]}  color='#1f1f1f' style={{marginLeft: '20px'}}/>];
+		item?.children?.map((child:any) => {
+			element = [...element, <Picker.Item key={child.id} label={child[fields.label]} value={child[fields.value]}  color='#1f1f1f' style={{marginLeft: '20px'}}/>];
 		})
 		
 	})
@@ -21,10 +21,12 @@ function itemsTree(props: any) {
 	return element;
 }
 
-function items(props: any) {
-	let element = [<Picker.Item label={props.placeholder} value='' color='#8b8b8b'/>];
-	props?.options?.map((item:any, index: number) => {
-		element = [...element, <Picker.Item key={index} label={item[props.fields.label]} value={item[props.fields.value]}  color='#1f1f1f'/>];
+function items({placeholder, options, fields}: any) {
+	
+	let element = [<Picker.Item label={placeholder} value='' color='#8b8b8b'/>];
+
+	options?.map((item:any) => {
+		element = [...element, <Picker.Item key={item.id} label={item[fields.label]} value={item[fields.value]}  color='#1f1f1f'/>];
 	})
 
 	return element;
