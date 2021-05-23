3ffafc7e3550b2c9ed80c361af7df87885637625
Kleber de Souza <kleber107@gmail.com>
2021-05-23 12:36:27 -0300

    Planejamento

--- src/pages/PlannedExpenses/index.tsx
+++ src/pages/PlannedExpenses/index.tsx
@@ -1,135 +1,146 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/core';
-import { update } from './services';
-import { IconText } from '../../components/elements/Icon';
-import { 
-    Container,
-    ContentScrollView,
-    Content,
-    Text,
-    TextTotal,
-    ContentTitle,
-    ContentPercent,
-    ContentTotal,
-    RowHr,
-    ViewPorcent,
-    BarPorcent,
-    Porcent,
-    Input,
-    TextPercent,
-    ViewContent,
-    CategoriesView
+import React, {useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/core';
+import {update} from './services';
+import {IconText} from '../../components/elements/Icon';
+import {
+	Container,
+	ContentScrollView,
+	Content,
+	Text,
+	TextTotal,
+	ContentTitle,
+	ContentPercent,
+	ContentTotal,
+	RowHr,
+	ViewPorcent,
+	BarPorcent,
+	Porcent,
+	Input,
+	TextPercent,
+	ViewContent,
+	CategoriesView,
 } from './style';
-import { getPlannedExpenses } from './services';
-import { Alert, StatusBar, View } from 'react-native';
+import {getPlannedExpenses} from './services';
+import {Alert, StatusBar, View} from 'react-native';
 
 export function PlannedExpenses() {
-    const navigate = useNavigation();
-    const [total, setTotal] = useState<number>(0);
-    let [categories, setCategories] = useState<any[]>([]);
+	const navigate = useNavigation();
+	const [total, setTotal] = useState<number>(0);
+	let [categories, setCategories] = useState<any[]>([]);
 
-    async function savePlannedExpenses(value: any, item: any) {
-        categories[categories.indexOf(item)].value_percent = value;
-        const copyCategories = [...categories];
+	async function savePlannedExpenses(value: any, item: any) {
+		categories[categories.indexOf(item)].value_percent = value;
+		const copyCategories = [...categories];
 
-        const isValid = await calcPorcent(copyCategories);
+		const isValid = await calcPorcent(copyCategories);
 
-        if (!isValid) {
-            Alert.alert("Valor total passou de 100%");
-        }
+		if (!isValid) {
+			Alert.alert('Valor total passou de 100%');
+		}
 
-        setCategories(copyCategories)
-        
+		setCategories(copyCategories);
 
-        const dados = await update(item);
+		const dados = await update(item);
+	}
+	async function listCategories() {
+		const dados = await getPlannedExpenses();
+		setCategories(dados);
+		calcPorcent(dados);
+	}
 
+	async function calcPorcent(dados: any[] = []) {
+		let total = 0;
 
-    }
-    async function listCategories() {
-        const dados = await getPlannedExpenses();
-        setCategories(dados);
-        calcPorcent(dados);
-    }
+		for (let item of dados) {
+			if (item.value_percent !== null && item.value_percent !== '') {
+				total += parseInt(item.value_percent);
+			}
+		}
 
-    async function calcPorcent(dados: any[] = []) {
-        let total = 0;
+		setTotal(isNaN(total) ? 0 : total);
 
-        for(let item of dados) {
-            if (item.value_percent !== null && item.value_percent !== '') {
-                total += parseInt(item.value_percent);
-            }
-        }
+		if (total > 100) {
+			return false;
+		}
+		return true;
+	}
 
-        
-        setTotal(isNaN(total) ? 0 : total);
-        
-        if (total > 100) {
-            return false;
-        }
-       return true;
-    }
+	useEffect(() => {
+		listCategories();
+		StatusBar.setBarStyle('light-content');
+		StatusBar.setBackgroundColor('#2C88D9');
+	}, []);
 
-    useEffect(() => {
-        listCategories();
-    }, []);
-    return (
-        <Container>
-            <StatusBar barStyle="light-content" backgroundColor="#2C88D9" /> 
-            <ViewContent>
-                <ContentScrollView>
-                    <View>
-                        {categories?.map((item: any, index: number) =>(
-                            <CategoriesView key={index}>
-                                <Content >
-                                    <ContentTitle>
-                                        <IconText name={item.icon}/>
-                                        <Text>{item.name}</Text>
-                                    </ContentTitle>
-                                    <ContentPercent>
-                                        <TextPercent>%</TextPercent>
-                                        <Input
-                                            value={item.value_percent ? `${item.value_percent}` : ""}
-                                            keyboardType="numeric"
-                                            maxLength = {3}
-                                            onChangeText={(text) => savePlannedExpenses(text, item)}
-                                        />
-                                    </ContentPercent>
-                                </Content>
-                                <RowHr/>
-                            </CategoriesView>
-                        ))}
-                    </View>
-                </ContentScrollView>
-                <ContentTotal>
-                    <BarPorcent>
-                        <Porcent style={{width: `${total > 100 ? 100 : total}%`}}></Porcent>
-                    </BarPorcent>
-                    <ViewPorcent style={{left: total >= 80 ? '80%' : `${total}%`, transform: [{translateX: -10}]}}>
-                        <TextTotal>{total}%</TextTotal>
-                    </ViewPorcent>
-                </ContentTotal>
-            </ViewContent>
-        </Container>
-    )
-};
+	return (
+		<Container>
+			<ViewContent>
+				<ContentScrollView>
+					<View>
+						{categories?.map((item: any, index: number) => (
+							<CategoriesView key={index}>
+								<Content>
+									<ContentTitle>
+										<IconText name={item.icon} />
+										<Text>{item.name}</Text>
+									</ContentTitle>
+									<ContentPercent>
+										<TextPercent>%</TextPercent>
+										<Input
+											value={
+												item.value_percent
+													? `${item.value_percent}`
+													: ''
+											}
+											keyboardType="numeric"
+											maxLength={3}
+											onChangeText={text =>
+												savePlannedExpenses(text, item)
+											}
+										/>
+									</ContentPercent>
+								</Content>
+								<RowHr />
+							</CategoriesView>
+						))}
+					</View>
+				</ContentScrollView>
+				<ContentTotal>
+					<BarPorcent>
+						<Porcent
+							style={{
+								width: `${total > 100 ? 100 : total}%`,
+							}}></Porcent>
+					</BarPorcent>
+					<ViewPorcent
+						style={{
+							left: total >= 80 ? '80%' : `${total}%`,
+							transform: [{translateX: -10}],
+						}}>
+						<TextTotal>{total}%</TextTotal>
+					</ViewPorcent>
+				</ContentTotal>
+			</ViewContent>
+		</Container>
+	);
+}
 
 const style = {
-    boxShadow: {
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 12,
-        },
+	boxShadow: {
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 12,
+		},
 
-        elevation: 2,
-    },
-    boxShadowInvoice: {
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 12,
-        },
+		elevation: 2,
+	},
+	boxShadowInvoice: {
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 12,
+		},
 
-        elevation: 1,
-    }
-}
+		elevation: 1,
+	},
+};
