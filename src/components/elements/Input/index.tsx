5968996d019a2ea40c13ba6a63d54a36a203b695
Kleber de Souza <kleber107@gmail.com>
2021-05-23 12:34:59 -0300

    input

--- src/components/elements/Input/index.tsx
+++ src/components/elements/Input/index.tsx
@@ -3,17 +3,22 @@ import {IconText} from '../Icon';
 
 import {Container, Input, BoxIcon, TextPreFixer} from './style';
 
-export function InputText( props: any) {
-
+export function InputText(props: any) {
 	return (
-		<Container style={{backgroundColor: props.backgroundColor, borderWidth: props.outline ? 1 : 0 , borderColor:  props.outline ? '#dadada' :  'none'}}>
+		<Container
+			style={{
+				backgroundColor: props.backgroundColor ?? '#e8e9ef',
+				borderWidth: props.outline ? 1 : 0,
+				borderColor: props.outline ? '#dadada' : 'none',
+			}}>
 			<BoxIcon>
-				{props.icon ? <IconText name={props.icon} /> : <TextPreFixer>{props?.preFixer}</TextPreFixer>}
+				{props.icon ? (
+					<IconText name={props.icon} />
+				) : (
+					<TextPreFixer>{props?.preFixer}</TextPreFixer>
+				)}
 			</BoxIcon>
-			<Input
-				{...props}
-				onChangeText={text => props.onChangeText(text)}
-			/>
+			<Input {...props} onChangeText={text => props.onChangeText(text)} />
 		</Container>
 	);
 }
