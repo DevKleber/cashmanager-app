3caa58915a8dd9f8a64001ad25bc463a7dc5e6c9
Kleber de Souza <kleber107@gmail.com>
2021-06-06 11:48:55 -0300

    eslint

--- src/pages/Transaction/services.ts
+++ src/pages/Transaction/services.ts
@@ -1,4 +1,7 @@
-import {api} from '../../services/api';
+/* eslint-disable camelcase */
+
+import { api } from '../../services/api';
+
 export interface TransactionProps {
 	id: number;
 	description: string;
@@ -14,13 +17,13 @@ export interface Month {
 	month: string;
 }
 
-export async function deleteAccount(id: number) {
-	const {data} = await api.delete(`/accounts/${id}`);
+export async function deleteAccount(id: number): Promise<any> {
+	const { data } = await api.delete(`/accounts/${id}`);
 	return data;
 }
 
-export async function getCategoriesByType(type: string = '') {
-	const {data} = await api.get(`/categories`, {
+export async function getCategoriesByType(type = '') {
+	const { data } = await api.get(`/categories`, {
 		params: {
 			type,
 		},
@@ -29,36 +32,36 @@ export async function getCategoriesByType(type: string = '') {
 }
 
 export async function getCategories() {
-	const {data} = await api.get(`/categories`);
+	const { data } = await api.get(`/categories`);
 	return data;
 }
 
 export async function save(form: any) {
-	const {data} = await api.post(`/transactions`, form);
+	const { data } = await api.post(`/transactions`, form);
 	return data;
 }
 
 export const optionsIsPaid = [
-	{label: 'Pago', value: true},
-	{label: 'A pagar', value: false},
+	{ label: 'Pago', value: true },
+	{ label: 'A pagar', value: false },
 ];
 
 export const optionsParcel = [
-	{label: '1x', value: '1'},
-	{label: '2x', value: '2'},
-	{label: '3x', value: '3'},
-	{label: '4x', value: '4'},
-	{label: '5x', value: '5'},
-	{label: '6x', value: '6'},
-	{label: '7x', value: '7'},
-	{label: '8x', value: '8'},
-	{label: '9x', value: '9'},
-	{label: '10x', value: '10'},
-	{label: '11x', value: '11'},
-	{label: '12x', value: '12'},
+	{ label: '1x', value: '1' },
+	{ label: '2x', value: '2' },
+	{ label: '3x', value: '3' },
+	{ label: '4x', value: '4' },
+	{ label: '5x', value: '5' },
+	{ label: '6x', value: '6' },
+	{ label: '7x', value: '7' },
+	{ label: '8x', value: '8' },
+	{ label: '9x', value: '9' },
+	{ label: '10x', value: '10' },
+	{ label: '11x', value: '11' },
+	{ label: '12x', value: '12' },
 ];
-export async function getTransactions(month: number = 0) {
-	const {data} = await api.get(`/transactions`, {
+export async function getTransactions(month = 0) {
+	const { data } = await api.get(`/transactions`, {
 		params: {
 			month,
 		},
