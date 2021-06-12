61f5ca83a1297c93da864878cd314de9b80d9065
Kleber de Souza <kleber107@gmail.com>
2021-06-12 17:24:10 -0300

    Criando error handle. Interceptando erros do sistema e mostrando mensagem.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,70 @@
-import axios from "axios";
-import environment from "./environment";
+import { Alert } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
+import environment from './environment';
+
+const token = async () => AsyncStorage.getItem('@CashManager:token');
 
 export const api = axios.create({
-  baseURL: environment.api,
+	baseURL: environment.api,
+	headers: {
+		Authorization: `Basic ${token}`,
+	},
 });
+
+api.interceptors.response.use(
+	response => response,
+	error => {
+		const { status, data } = error.response;
+
+		switch (status) {
+			case 500:
+				Alert.alert(data.message);
+				break;
+			case 400:
+				switch (data.error) {
+					case `"Token·is·Invalid"`:
+						Alert.alert(data.error);
+						break;
+					default:
+						if (data.message) {
+							Alert.alert(data.message);
+						} else {
+							Alert.alert(data.response);
+						}
+
+						break;
+				}
+
+				break;
+
+			case 401:
+				if (data.response) {
+					Alert.alert(data.message);
+				}
+
+				if (data.message) {
+					Alert.alert(data.message);
+				}
+				if (data.error) {
+					if (data.error === 'Unauthorized') {
+						Alert.alert('Usuário ou senha inválida!');
+					} else {
+						Alert.alert(data.message);
+					}
+				}
+
+				break;
+			case 405:
+				Alert.alert(data.message);
+
+				break;
+			default:
+				break;
+		}
+
+		throw error;
+	},
+);
