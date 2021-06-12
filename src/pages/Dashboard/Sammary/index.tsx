ab6e1ef05d29cc2c8d10abd2145ebbf2af2635bc
Kleber de Souza <kleber107@gmail.com>
2021-06-12 17:21:47 -0300

    eslint

--- src/pages/Dashboard/Sammary/index.tsx
+++ src/pages/Dashboard/Sammary/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text } from 'react-native';
 import NumberFormat from 'react-number-format';
-import {IconText} from '../../../components/elements/Icon';
-import {Summary, Header, Content, Card, Name, Value, De} from './style';
+
+import { IconText } from '../../../components/elements/Icon';
+
+import { Summary, Header, Content, Card, Name, Value, De } from './style';
 
 interface SammaryProps {
 	isTransaction?: boolean;
@@ -10,12 +12,7 @@ interface SammaryProps {
 	middleCard: any;
 	lastCard: any;
 }
-export function Sammary({
-	isTransaction,
-	fisrtCard,
-	middleCard,
-	lastCard,
-}: SammaryProps) {
+export function Sammary({ isTransaction, fisrtCard, middleCard, lastCard }: SammaryProps): JSX.Element {
 	function getColorTotal(isTransaction: any, value: number): string {
 		if (isTransaction && value >= 0) {
 			return '#00EB84';
@@ -37,8 +34,8 @@ export function Sammary({
 				<Content>
 					<NumberFormat
 						value={fisrtCard.value}
-						prefix={'R$ '}
-						displayType={'text'}
+						prefix="R$ "
+						displayType="text"
 						thousandSeparator="."
 						decimalSeparator=","
 						renderText={value => <Value>{value}</Value>}
@@ -53,8 +50,8 @@ export function Sammary({
 				<Content>
 					<NumberFormat
 						value={middleCard.value}
-						prefix={'R$ '}
-						displayType={'text'}
+						prefix="R$ "
+						displayType="text"
 						thousandSeparator="."
 						decimalSeparator=","
 						renderText={value => <Value>{value}</Value>}
@@ -69,42 +66,29 @@ export function Sammary({
 				<Header>
 					<Name
 						style={{
-							color: getColorTotal(
-								isTransaction,
-								lastCard?.value,
-							),
+							color: getColorTotal(isTransaction, lastCard?.value),
 						}}>
 						{lastCard.title}
 					</Name>
-					<IconText
-						name="outlined-flag"
-						color={getColorTotal(isTransaction, lastCard?.value)}
-						size={27}
-					/>
+					<IconText name="outlined-flag" color={getColorTotal(isTransaction, lastCard?.value)} size={27} />
 				</Header>
 				<Content>
 					{/* <Value style={{color: isTransaction ? '#E62E4D' : '#fff'}}> */}
 					<Value
 						style={{
-							color: getColorTotal(
-								isTransaction,
-								lastCard?.value,
-							),
+							color: getColorTotal(isTransaction, lastCard?.value),
 						}}>
 						{isTransaction ? (
 							<NumberFormat
 								value={lastCard?.value}
-								prefix={'R$ '}
-								displayType={'text'}
+								prefix="R$ "
+								displayType="text"
 								thousandSeparator="."
 								decimalSeparator=","
 								renderText={value => (
 									<Value
 										style={{
-											color: getColorTotal(
-												isTransaction,
-												lastCard?.value,
-											),
+											color: getColorTotal(isTransaction, lastCard?.value),
 										}}>
 										{value}
 									</Value>
@@ -112,8 +96,7 @@ export function Sammary({
 							/>
 						) : (
 							<>
-								{lastCard?.value?.total.toFixed(1)}%
-								<De> de </De>
+								{lastCard?.value?.total.toFixed(1)}%<De> de </De>
 								{lastCard?.value?.totalPlanejado}%
 							</>
 						)}
@@ -127,7 +110,7 @@ export function Sammary({
 const style = {
 	boxShadow: {
 		shadowColor: '#000',
-		shadowOffset: {width: 0, height: 4},
+		shadowOffset: { width: 0, height: 4 },
 		shadowRadius: 4,
 		shadowOpacity: 0.25,
 		elevation: 10,
