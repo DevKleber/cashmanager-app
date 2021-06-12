54483e06538cd14f7d8da6e29b1b17b5f33c5ba2
Kleber de Souza <kleber107@gmail.com>
2021-06-12 17:22:41 -0300

    eslint

--- src/pages/Dashboard/services.ts
+++ src/pages/Dashboard/services.ts
@@ -1,6 +1,6 @@
-import {api} from '../../services/api';
+import { api } from '../../services/api';
 
-export async function getDashboardData() {
-	const {data} = await api.get(`/dashboard`);
+export async function getDashboardData(): Promise<any> {
+	const { data } = await api.get(`/dashboard`);
 	return data;
 }
