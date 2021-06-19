86e827ee969272e8cbad14d38eb33e6df93ebec4
Kleber de Souza <kleber107@gmail.com>
2021-06-19 18:15:39 -0300

    melhorias css

--- src/pages/Account/AccountInsert/style.ts
+++ src/pages/Account/AccountInsert/style.ts
@@ -1,33 +1,39 @@
+import { Platform } from 'react-native';
+
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView`
 	flex: 1;
-	width: 100%;
+	height: 100%;
+	/* width: 100%;
 	align-items: center;
-	flex-direction: column;
-	background-color: #F7C325;
+	flex-direction: column; */
+	background-color: #f7c325;
 `;
 
 export const ContentScrollView = styled.ScrollView`
 	width: 100%;
-	padding: 0px 25px;
-	margin-top: 20px;
-	padding-top: 90px;
-	margin-top: 110px;
+
+	padding: 90px 25px 204px 25px;
+	margin-top: 50px;
+
 	border-top-left-radius: 45px;
 	border-top-right-radius: 45px;
 	background-color: #fff;
 `;
 
 export const BtnNewCard = styled.TouchableOpacity`
-	width: 100%;
 	align-items: center;
+	justify-content: center;
+
+	width: 100%;
 	flex-direction: row;
-	background-color: #F7C325;
-	color: #2a004f;
-	border-radius: 10px;
 	margin-top: 10px;
 	padding: 15px;
+
+	background-color: #f7c325;
+	color: #2a004f;
+	border-radius: 10px;
 	font-size: 15px;
 `;
 
