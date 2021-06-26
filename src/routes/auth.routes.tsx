94cd8b6c1fd80e0ba1111ce8e7acb05f3fea35ca
Kleber de Souza <kleber107@gmail.com>
2021-06-26 13:08:10 -0300

    colocando tela de splash como padrao

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import { StatusBar } from 'react-native';
 
-import {Splash} from './../pages/Splash';
-import {Login} from './../pages/Login';
-import {NewAccount} from '../pages/NewAccount';
-import {StatusBar} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import { ForgotPassword } from '../pages/ForgotPassword';
+import { Login } from '../pages/Login';
+import { NewAccount } from '../pages/NewAccount';
+import { Splash } from '../pages/Splash';
 
 const Auth = createStackNavigator();
 
-export default function AuthRoutes() {
+export default function AuthRoutes(): JSX.Element {
 	return (
 		<>
 			<StatusBar barStyle="dark-content" backgroundColor="#F5F6FC" />
 			<Auth.Navigator
 				screenOptions={{
 					headerShown: false,
-					cardStyle: {backgroundColor: '#fff'},
-					
+					cardStyle: { backgroundColor: '#fff' },
 				}}>
+				<Auth.Screen name="Splash" component={Splash} />
 				<Auth.Screen name="SignIn" component={Login} />
 				<Auth.Screen name="SignUp" component={NewAccount} />
-				<Auth.Screen name="Splash" component={Splash} />
 				<Auth.Screen name="ForgotPassword" component={ForgotPassword} />
 			</Auth.Navigator>
 		</>
