dfed22ea05091ce79338935c8df1092d181449b6
Kleber de Souza <kleber107@gmail.com>
2021-06-26 13:07:41 -0300

    add  splash com tempo de 2 seg caso não logado

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,23 +1,26 @@
-import {useNavigation} from '@react-navigation/core';
-import React, {useEffect} from 'react';
-import {Image} from 'react-native';
-import {StyleSheet} from 'react-native';
+import React, { useEffect } from 'react';
+import { Image, StyleSheet } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
-export function Splash() {
+import { useNavigation } from '@react-navigation/core';
+
+import { useAuth } from '../../hooks/Auth';
+
+export function Splash(): JSX.Element {
 	const navigation = useNavigation();
+	const { user } = useAuth();
 
 	useEffect(() => {
-		
-	}, []);
+		if (!user) {
+			setTimeout(() => {
+				navigation.navigate('SignIn');
+			}, 2000);
+		}
+	}, [user, navigation]);
 
 	return (
-		<LinearGradient
-			colors={['#fff', '#00EB8453']}
-			style={styles.linearGradient}>
-			<Image
-				source={require('./../../assets/img/logoSplashbranco.png')}
-			/>
+		<LinearGradient colors={['#fff', '#00EB8453']} style={styles.linearGradient}>
+			<Image source={require('../../assets/img/logoSplashbranco.png')} />
 		</LinearGradient>
 	);
 }
